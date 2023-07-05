65d7b7ecf96cd619a8773a24379b6356b8b5aec7
Ming Tan <tango.11@yahoo.com>
2023-07-05 11:50:49 +0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -872,7 +872,7 @@ vim.opt.list = true
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
-    show_current_context_start = true,
+    -- show_current_context_start = true,
     -- char_highlight_list = {
     --     "IndentBlanklineIndent1",
     --     "IndentBlanklineIndent2",

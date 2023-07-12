28d599c7223a288d7386bf64f04f5045113ea3d1
Ming Tan <tango.11@yahoo.com>
2023-07-12 17:36:32 +0700

    change fast load

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -425,16 +425,7 @@ require('lazy').setup({
   -- load faster
   {'lewis6991/impatient.nvim'},
   --codeium AI
-  {
-   'Exafunction/codeium.vim',
-    config = function ()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
-      vim.keymap.set('i', '<C-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
-      vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
-      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
-    end
-  },
+  {'Exafunction/codeium.vim'},
   {'xiyaowong/transparent.nvim'},
 
   { import = 'custom.plugins' },

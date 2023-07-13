34255c8160610be7a415206d92cf8a17727a61a6
Ming Tan <tango.11@yahoo.com>
2023-07-13 14:12:43 +0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -414,7 +414,14 @@ require('lazy').setup({
   -- load faster
   {'lewis6991/impatient.nvim'},
   --codeium AI
-  {'Exafunction/codeium.vim'},
+  {  'Exafunction/codeium.vim',
+  config = function ()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<C-g>', function () return vim.fn['codeium#Accept']() end, { expr = true })
+    vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
+    vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
+    vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true })
+  end},
   {'xiyaowong/transparent.nvim'},
 
   { import = 'custom.plugins' },

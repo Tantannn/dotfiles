4e420ab85a623b1cb2c95dcb3d3283a1ff1b6521
Ming Tan <tango.11@yahoo.com>
2023-07-18 23:57:21 +0700

    Update comment tsx,jsx

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -182,11 +182,10 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim',
     dependencies = {
-      'JoosepAlviste/nvim-ts-context-commentstring',
     },
     opts = {}
   },
-
+  {'JoosepAlviste/nvim-ts-context-commentstring'},
   -- Fuzzy Finder (files, lsp, etc)
   { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
@@ -555,6 +554,7 @@ require('nvim-treesitter.configs').setup {
 
   context_commentstring = {
     enable = true,
+    enable_autocmd = false,
   },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
@@ -1012,6 +1012,96 @@ eslint.setup({
 vim.cmd('hi nvimtreeendofbuffer guibg=none ctermbg=none')
 vim.cmd[[hi nvimtreenormal guibg=none ctermbg=none]]
 
+--barbecue custom theme
+-- require("barbecue").setup({
+--   theme = {
+--     -- this highlight is used to override other highlights
+--     -- you can take advantage of its `bg` and set a background throughout your winbar
+--     -- (e.g. basename will look like this: { fg = "#c0caf5", bold = true })
+--     normal = { fg = "#c0caf5" }
+--
+--     -- these highlights correspond to symbols table from config
+--     -- ellipsis = { fg = "#737aa2" },
+--     -- separator = { fg = "#737aa2" },
+--     -- modified = { fg = "#737aa2" },
+--     --
+--     -- these highlights represent the _text_ of three main parts of barbecue
+--     -- dirname = { fg = "#737aa2" },
+--     -- basename = { bold = true },
+--     -- context = {},
+--
+--     -- these highlights are used for context/navic icons
+--     -- context_file = { fg = "#ac8fe4" },
+--     -- context_module = { fg = "#ac8fe4" },
+--     -- context_namespace = { fg = "#ac8fe4" },
+--     -- context_package = { fg = "#ac8fe4" },
+--     -- context_class = { fg = "#ac8fe4" },
+--     -- context_method = { fg = "#ac8fe4" },
+--     -- context_property = { fg = "#ac8fe4" },
+--     -- context_field = { fg = "#ac8fe4" },
+--     -- context_constructor = { fg = "#ac8fe4" },
+--     -- context_enum = { fg = "#ac8fe4" },
+--     -- context_interface = { fg = "#ac8fe4" },
+--     -- context_function = { fg = "#ac8fe4" },
+--     -- context_variable = { fg = "#ac8fe4" },
+--     -- context_constant = { fg = "#ac8fe4" },
+--     -- context_string = { fg = "#ac8fe4" },
+--     -- context_number = { fg = "#ac8fe4" },
+--     -- context_boolean = { fg = "#ac8fe4" },
+--     -- context_array = { fg = "#ac8fe4" },
+--     -- context_object = { fg = "#ac8fe4" },
+--     -- context_key = { fg = "#ac8fe4" },
+--     -- context_null = { fg = "#ac8fe4" },
+--     -- context_enum_member = { fg = "#ac8fe4" },
+--     -- context_struct = { fg = "#ac8fe4" },
+--     -- context_event = { fg = "#ac8fe4" },
+--     -- context_operator = { fg = "#ac8fe4" },
+--     -- context_type_parameter = { fg = "#ac8fe4" },
+--     --
+--   },
+-- })
+--
+-- require("nvim-navic").setup({
+--     icons = {
+--         File          = "󰈙 ",
+--         Module        = " ",
+--         Namespace     = "󰌗 ",
+--         Package       = " ",
+--         Class         = "󰌗 ",
+--         Method        = "󰆧 ",
+--         Property      = " ",
+--         Field         = " ",
+--         Constructor   = " ",
+--         Enum          = "󰕘",
+--         Interface     = "󰕘",
+--         Function      = "󰊕 ",
+--         Variable      = "󰆧 ",
+--         Constant      = "󰏿 ",
+--         String        = "󰀬 ",
+--         Number        = "󰎠 ",
+--         Boolean       = "◩ ",
+--         Array         = "󰅪 ",
+--         Object        = "󰅩 ",
+--         Key           = "󰌋 ",
+--         Null          = "󰟢 ",
+--         EnumMember    = " ",
+--         Struct        = "󰌗 ",
+--         Event         = " ",
+--         Operator      = "󰆕 ",
+--         TypeParameter = "󰊄 ",
+--     },
+--     -- lsp = {
+--     --     auto_attach = false,
+--     --     preference = nil,
+--     -- },
+--     highlight = false,
+--     separator = " > ",
+--     depth_limit = 0,
+--     depth_limit_indicator = "..",
+--     safe_output = true,
+--     click = false
+-- })
+
 -- css color 
 vim.keymap.set("n", "<C-q>", "<cmd>PickColor<cr>", opts)
 vim.keymap.set("i", "<C-q>", "<cmd>PickColorInsert<cr>", opts)
@@ -1037,6 +1127,11 @@ require("color-picker").setup({ -- for changing icons & mappings
 })
 
 vim.cmd([[hi FloatBorder guibg=NONE]])
+
+--comment
+require('Comment').setup {
+  pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+}
 -- Other:
 -- :BarbarEnable - enables barbar (enabled by default)
 -- :BarbarDisable - very bad command, should never be used- :BarbarDisable - very bad command, should never be used

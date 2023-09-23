10d1f78f3bad82d29b83e147bfaa7f26ad34a72b
Ming Tan <tango.11@yahoo.com>
2023-09-23 09:16:28 +0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -502,6 +502,305 @@ require('lazy').setup({
   { 'chentoast/marks.nvim' },
   -- toggle Terminal
   {'akinsho/toggleterm.nvim', version = "*", config = true},
+  -- hardtime
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {  max_time = 1000,
+      max_count = 2,
+      disable_mouse = true,
+      hint = true,
+      notification = true,
+      allow_different_key = false,
+      enabled = true,
+      resetting_keys = {
+        ["1"] = { "n", "x" },
+        ["2"] = { "n", "x" },
+        ["3"] = { "n", "x" },
+        ["4"] = { "n", "x" },
+        ["5"] = { "n", "x" },
+        ["6"] = { "n", "x" },
+        ["7"] = { "n", "x" },
+        ["8"] = { "n", "x" },
+        ["9"] = { "n", "x" },
+        ["c"] = { "n" },
+        ["C"] = { "n" },
+        ["d"] = { "n" },
+        ["x"] = { "n" },
+        ["X"] = { "n" },
+        ["y"] = { "n" },
+        ["Y"] = { "n" },
+        ["p"] = { "n" },
+        ["P"] = { "n" },
+      },
+      restriction_mode = "block", -- block or hint
+      restricted_keys = {
+        ["h"] = { "n", "x" },
+        ["j"] = { "n", "x" },
+        ["k"] = { "n", "x" },
+        ["l"] = { "n", "x" },
+        ["-"] = { "n", "x" },
+        ["+"] = { "n", "x" },
+        ["gj"] = { "n", "x" },
+        ["gk"] = { "n", "x" },
+        ["<CR>"] = { "n", "x" },
+        ["<C-M>"] = { "n", "x" },
+        ["<C-N>"] = { "n", "x" },
+        ["<C-P>"] = { "n", "x" },
+      },
+      disabled_keys = {
+        ["<Up>"] = { "", "i" },
+        ["<Down>"] = { "", "i" },
+        ["<Left>"] = { "", "i" },
+        ["<Right>"] = { "", "i" },
+      },
+      disabled_filetypes = { "qf", "netrw", "NvimTree", "lazy", "mason" },
+      hints = {
+        ["[kj][%^_]"] = {
+          message = function(key)
+            return "Use "
+              .. (key:sub(1, 1) == "k" and "-" or "<CR> or +")
+              .. " instead of "
+              .. key
+          end,
+          length = 2,
+        },
+        ["%$a"] = {
+          message = function()
+            return "Use A instead of $a"
+          end,
+          length = 2,
+        },
+        ["%^i"] = {
+          message = function()
+            return "Use I instead of ^i"
+          end,
+          length = 2,
+        },
+        ["%D[k-]o"] = {
+          message = function(keys)
+            return "Use O instead of " .. keys:sub(2)
+          end,
+          length = 3,
+        },
+        ["%D[j+]O"] = {
+          message = function(keys)
+            return "Use o instead of " .. keys:sub(2)
+          end,
+          length = 3,
+        },
+        ["[^fFtT]li"] = {
+          message = function()
+            return "Use a instead of li"
+          end,
+          length = 3,
+        },
+        ["2([dcy=<>])%1"] = {
+          message = function(key)
+            return "Use " .. key:sub(3) .. "j instead of " .. key
+          end,
+          length = 3,
+        },
+
+        -- hints for f/F/t/T
+        ["[^dcy=]f.h"] = {
+          message = function(keys)
+            return "Use t" .. keys:sub(3, 3) .. " instead of " .. keys:sub(2)
+          end,
+          length = 4,
+        },
+        ["[^dcy=]F.l"] = {
+          message = function(keys)
+            return "Use T" .. keys:sub(3, 3) .. " instead of " .. keys:sub(2)
+          end,
+          length = 4,
+        },
+        ["[^dcy=]T.h"] = {
+          message = function(keys)
+            return "Use F" .. keys:sub(3, 3) .. " instead of " .. keys:sub(2)
+          end,
+          length = 4,
+        },
+        ["[^dcy=]t.l"] = {
+          message = function(keys)
+            return "Use f" .. keys:sub(3, 3) .. " instead of " .. keys:sub(2)
+          end,
+          length = 4,
+        },
+
+        -- hints for delete + insert
+        ["d[bBwWeE%^%$]i"] = {
+          message = function(keys)
+            return "Use " .. "c" .. keys:sub(2, 2) .. " instead of " .. keys
+          end,
+          length = 3,
+        },
+        ["dg[eE]i"] = {
+          message = function(keys)
+            return "Use " .. "c" .. keys:sub(2, 3) .. " instead of " .. keys
+          end,
+          length = 4,
+        },
+        ["d[tTfF].i"] = {
+          message = function(keys)
+            return "Use " .. "c" .. keys:sub(2, 3) .. " instead of " .. keys
+          end,
+          length = 4,
+        },
+        ["d[ia][\"'`{}%[%]()<>bBwWspt]i"] = {
+          message = function(keys)
+            return "Use " .. "c" .. keys:sub(2, 3) .. " instead of " .. keys
+          end,
+          length = 4,
+        },
+
+        -- hints for unnecessary visual mode
+        ["Vgg[dcy=<>]"] = {
+          message = function(keys)
+            return "Use " .. keys:sub(4, 4) .. "gg instead of " .. keys
+          end,
+          length = 4,
+        },
+        ['Vgg".[dy]'] = {
+          message = function(keys)
+            return "Use " .. keys:sub(4, 6) .. "gg instead of " .. keys
+          end,
+          length = 6,
+        },
+        ["VG[dcy=<>]"] = {
+          message = function(keys)
+            return "Use " .. keys:sub(3, 3) .. "G instead of " .. keys
+          end,
+          length = 3,
+        },
+        ['VG".[dy]'] = {
+          message = function(keys)
+            return "Use " .. keys:sub(3, 5) .. "G instead of " .. keys
+          end,
+          length = 5,
+        },
+        ["V%d[kj][dcy=<>]"] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 4)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 4,
+        },
+        ['V%d[kj]".[dy]'] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 6)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 6,
+        },
+        ["V%d%d[kj][dcy=<>]"] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(5, 5)
+              .. keys:sub(2, 4)
+              .. " instead of "
+              .. keys
+          end,
+          length = 5,
+        },
+        ['V%d%d[kj]".[dy]'] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(5, 7)
+              .. keys:sub(2, 4)
+              .. " instead of "
+              .. keys
+          end,
+          length = 7,
+        },
+        ["[vV][bBwWeE%^%$][dcy=<>]"] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(3, 3)
+              .. keys:sub(2, 2)
+              .. " instead of "
+              .. keys
+          end,
+          length = 3,
+        },
+        ['[vV][bBwWeE%^%$]".[dy]'] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(3, 5)
+              .. keys:sub(2, 2)
+              .. " instead of "
+              .. keys
+          end,
+          length = 5,
+        },
+        ["[vV]g[eE][dcy=<>]"] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 4)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 4,
+        },
+        ['[vV]g[eE]".[dy]'] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 6)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 6,
+        },
+        ["[vV][tTfF].[dcy=<>]"] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 4)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 4,
+        },
+        ['[vV][tTfF].".[dy]'] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 6)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 6,
+        },
+        ["[vV][ia][\"'`{}%[%]()<>bBwWspt][dcy=<>]"] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 4)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 4,
+        },
+        ['[vV][ia]["\'`{}%[%]()<>bBwWspt]".[dy]'] = {
+          message = function(keys)
+            return "Use "
+              .. keys:sub(4, 6)
+              .. keys:sub(2, 3)
+              .. " instead of "
+              .. keys
+          end,
+          length = 6,
+        },
+      },}
+  },
 
   { import = 'custom.plugins' },
 }, {})
@@ -1002,8 +1301,13 @@ vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', {desc = "Resize wind
 -- inoremap <silent><c-t> <Esc><Cmd>exe v:count1 . "ToggleTerm"<CR>
 vim.keymap.set('n', '<C-t>', ':ToggleTerm<CR>', {desc = "Toggle Term"});
 vim.keymap.set('i', '<C-t>', ':ToggleTerm<CR>', {desc = "Toggle Term"});
-vim.keymap.set('t', '<C-t>', ':ToggleTerm<CR>', {desc = "Toggle Term"});
-
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
 --nvim tree
 require("nvim-tree").setup({
   filters = {
@@ -1436,7 +1740,7 @@ local config = {
       normal = { c = { fg = colors.fg, bg = colors.bg } },
       inactive = { c = { fg = colors.fg, bg = colors.bg } }, 
       component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
     },
   },
   sections = {

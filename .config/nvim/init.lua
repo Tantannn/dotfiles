fb324fb77045c7239da1fd5354334d2b69f12c1c
Ming Tan <tango.11@yahoo.com>
2023-09-26 09:51:00 +0700

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1080,6 +1080,22 @@ local servers = {
   },
 }
 
+local pid = vim.fn.getpid()
+-- On linux/darwin if using a release build, otherwise under scripts/OmniSharp(.Core)(.cmd)
+local omnisharp_bin = "/home/tantannn/.cache/omnisharp-vim/omnisharp-roslyn/run"
+-- on Windows
+-- local omnisharp_bin = "/path/to/omnisharp/OmniSharp.exe"
+
+local config = {
+  handlers = {
+    ["textDocument/definition"] = require('omnisharp_extended').handler,
+  },
+  cmd = { omnisharp_bin, '--languageserver' , '--hostPID', tostring(pid) },
+  -- rest of your settings
+}
+
+require'lspconfig'.omnisharp.setup(config)
+
 -- Setup neovim lua configuration
 require('neodev').setup()
 
